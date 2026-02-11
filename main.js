8747aa9656c1d8eb4664f8fe9da7ccb7068fdce3
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-11 15:05:11 +0100

    refactor: simplify lamp toggle with single button and image

--- main.js
+++ main.js
@@ -13,33 +13,24 @@ Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
 Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
 E così via...
 
-*/ 
+*/
 
-const buttonOnEl = document.querySelector('.turn_on');
-console.log(buttonOnEl);
+const buttonEl = document.querySelector("button");
+console.log(buttonEl);
 
-const buttonOffEl = document.querySelector('.turn_off')
-console.log(buttonOffEl);
+const imgEl = document.querySelector("img");
+console.log(imgEl);
 
-const imgOffEl = document.querySelector('.lamp_off');
-console.log(imgOffEl);
 
-const imgOnEl = document.querySelector('.lamp_on');
-console.log(imgOnEl);
+buttonEl.addEventListener("click", function () {
+  if (this.textContent.includes('Accendi')) {
+    buttonEl.textContent = "Spegni";
+    imgEl.src = "img/yellow_lamp.png";
+  }
+  else{
+    buttonEl.textContent = "Accendi";
+    imgEl.src = "img/white_lamp.png";
+  }
+});
 
-buttonOnEl.addEventListener('click', function(){
-    imgOffEl.style.display = 'none'
-    imgOnEl.style.display = 'inline-block';
-    buttonOnEl.style.display = 'none'
-    buttonOffEl.style.display = 'inline-block'
-    console.log('You turned on the light');
-})
 
-buttonOffEl.addEventListener('click', function(){
-    imgOffEl.style.display = 'inline-block';
-    imgOnEl.style.display = 'none'
-    buttonOffEl.style.display = 'none'
-    buttonOnEl.style.display = 'inline-block'
-    console.log('You turned off the light');
-    
-})
